919d606dbec196155f54001900f4549cb75b7264
Sergio Alejandro Grimaldo Rincon <sergio.grimaldo@thomasgreg.com>
2023-07-11 11:04:16 -0500

    Avance

---
+++ PanelPrincipalForm.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace AudioMonitor
+{
+    partial class PanelPrincipalForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            pictureBox1 = new PictureBox();
+            button1 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            listBox1 = new ListBox();
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            groupBox1.Controls.Add(listBox1);
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Controls.Add(pictureBox1);
+            groupBox1.Location = new Point(12, 12);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(776, 426);
+            groupBox1.TabIndex = 5;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Device";
+            groupBox1.Enter += groupBox1_Enter;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.LOGO_UNIAGUSTINIANA;
+            pictureBox1.Location = new Point(32, 61);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(255, 330);
+            pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(632, 267);
+            button1.Name = "button1";
+            button1.Size = new Size(64, 25);
+            button1.TabIndex = 6;
+            button1.Text = ">>";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(319, 143);
+            label1.Name = "label1";
+            label1.Size = new Size(433, 37);
+            label1.TabIndex = 7;
+            label1.Text = "Bienvenido al registro de asistencia";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(391, 214);
+            label2.Name = "label2";
+            label2.Size = new Size(280, 25);
+            label2.TabIndex = 8;
+            label2.Text = "Por favor, seleccione su nombre";
+            // 
+            // listBox1
+            // 
+            listBox1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 21;
+            listBox1.Items.AddRange(new object[] { "Estudiante...", "Orlando Divandoque", "Sebastian Garcés", "Sergio Grimaldos", "Laura López", "Sergio López" });
+            listBox1.Location = new Point(365, 267);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(261, 25);
+            listBox1.TabIndex = 9;
+            listBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged_1;
+            // 
+            // PanelPrincipalForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(groupBox1);
+            Name = "PanelPrincipalForm";
+            Text = "PanelPrincipalForm";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox groupBox1;
+        private Button button1;
+        private PictureBox pictureBox1;
+        private Label label1;
+        private ListBox listBox1;
+        private Label label2;
+    }
+}
---
+++ PanelPrincipalForm.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AudioMonitor
+{
+    public partial class PanelPrincipalForm : Form
+    {
+        public PanelPrincipalForm()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
