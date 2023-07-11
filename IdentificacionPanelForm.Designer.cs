fa53d87f5f5563d51925ec5fbe57364c812b88d0
Sergio Alejandro Grimaldo Rincon <sergio.grimaldo@thomasgreg.com>
2023-07-11 11:13:30 -0500

    Avance

--- IdentificacionPanelForm.Designer.cs
+++ IdentificacionPanelForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace AudioMonitor
 {
-    partial class PanelPrincipalForm
+    partial class IdentificacionPanelForm
     {
         /// <summary>
         /// Required designer variable.
@@ -29,57 +29,38 @@
         private void InitializeComponent()
         {
             groupBox1 = new GroupBox();
-            listBox1 = new ListBox();
             button1 = new Button();
             label2 = new Label();
             label1 = new Label();
-            pictureBox1 = new PictureBox();
             groupBox1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // groupBox1
             // 
             groupBox1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
-            groupBox1.Controls.Add(listBox1);
             groupBox1.Controls.Add(button1);
             groupBox1.Controls.Add(label2);
             groupBox1.Controls.Add(label1);
-            groupBox1.Controls.Add(pictureBox1);
             groupBox1.Location = new Point(12, 12);
             groupBox1.Name = "groupBox1";
             groupBox1.Size = new Size(776, 426);
-            groupBox1.TabIndex = 5;
+            groupBox1.TabIndex = 6;
             groupBox1.TabStop = false;
-            groupBox1.Enter += groupBox1_Enter;
-            // 
-            // listBox1
-            // 
-            listBox1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            listBox1.FormattingEnabled = true;
-            listBox1.ItemHeight = 21;
-            listBox1.Items.AddRange(new object[] { "Estudiante...", "Orlando Divandoque", "Sebastian Garcés", "Sergio Grimaldos", "Laura López", "Sergio López" });
-            listBox1.Location = new Point(365, 267);
-            listBox1.Name = "listBox1";
-            listBox1.Size = new Size(261, 25);
-            listBox1.TabIndex = 9;
-            listBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged_1;
             // 
             // button1
             // 
-            button1.Location = new Point(632, 267);
+            button1.Location = new Point(630, 332);
             button1.Name = "button1";
             button1.Size = new Size(64, 25);
             button1.TabIndex = 6;
             button1.Text = ">>";
             button1.UseVisualStyleBackColor = true;
-            button1.Click += button1_Click;
             // 
             // label2
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
-            label2.Location = new Point(391, 214);
+            label2.Location = new Point(20, 93);
             label2.Name = "label2";
             label2.Size = new Size(280, 25);
             label2.TabIndex = 8;
@@ -89,34 +70,23 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point);
-            label1.Location = new Point(319, 143);
+            label1.Location = new Point(20, 29);
             label1.Name = "label1";
-            label1.Size = new Size(433, 37);
+            label1.Size = new Size(147, 37);
             label1.TabIndex = 7;
-            label1.Text = "Bienvenido al registro de asistencia";
+            label1.Text = "Estudiante:";
+            label1.Click += label1_Click;
             // 
-            // pictureBox1
-            // 
-            pictureBox1.Image = Properties.Resources.LOGO_UNIAGUSTINIANA;
-            pictureBox1.Location = new Point(32, 61);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(255, 330);
-            pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
-            pictureBox1.TabIndex = 0;
-            pictureBox1.TabStop = false;
-            pictureBox1.Click += pictureBox1_Click;
-            // 
-            // PanelPrincipalForm
+            // IdentificacionPanelForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
             Controls.Add(groupBox1);
-            Name = "PanelPrincipalForm";
-            Text = "PanelPrincipalForm";
+            Name = "IdentificacionPanelForm";
+            Text = "IdentificacionPanelForm";
             groupBox1.ResumeLayout(false);
             groupBox1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
         }
 
@@ -124,9 +94,7 @@
 
         private GroupBox groupBox1;
         private Button button1;
-        private PictureBox pictureBox1;
-        private Label label1;
-        private ListBox listBox1;
         private Label label2;
+        private Label label1;
     }
 }
